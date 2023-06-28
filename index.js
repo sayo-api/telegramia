922e3e117db3922ce234fe5c62bdf8c2af078063
syp <focograal@gmail.com>
2023-06-27 23:51:34 -0300

    first commit

--- index.js
+++ index.js
@@ -1,43 +1,16 @@
-require("./config");
 const {IgApiClient} = require("instagram-private-api");
 const {withRealtime} = require("instagram_mqtt");
 const fs = require("fs");
-const pkg = require("request-promise");
-const schedule = require('node-schedule');
-const axios = require('axios');
-const cheerio = require("cheerio");
+const delay = require('delay');
+const ig = withRealtime(new IgApiClient())
 const express = require('express');
-const { createReadStream } = require("fs");
-const { Configuration, OpenAIApi } = require("openai");
+const ms = require('ms');
+
 
 const app = express();
-app.use(express.static('public'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-
-const { oggToMp3, streamToText } = require("./lib/convertVoice");
-const { antiSpam } = require('./lib/antispam')
-const { removeStreams } = require("./lib/helpers");
 
 
-const { viverdb } = require('./banco/1');
-const { usuario, bot_inf, grupos } = require('./banco/2');
-const { gerarUsuario, addBotinfo, veriNumero, verificaNome, verificaAll, semlimit, equilibrio, darlimit, niveladd, addcash, delcash, checkPremium, deletar_premium, adicionar_premium, tempo_expirado, expadd, veriCpf, addcashpix, delcashpix, verificaAllcpf } = require('./banco/3');
-
-const { randomNumber } = require('./lib/necessita');
-
-//iniciando banco de dados
-viverdb() 
-
-const { get } = pkg;
-
-const configuration = new Configuration({
-	apiKey: "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",
-});
-const openai = new OpenAIApi(configuration);
-
-const ig = withRealtime(new IgApiClient())
-ig.state.generateDevice("kasumi");
+ig.state.generateDevice("kzm");
 
 
 function saveState(data) {
@@ -56,248 +29,75 @@ function saveState(data) {
   }
   
   
-function pensador(nome) {
-return new Promise((resolve, reject) => {
-  axios.get(`https://www.pensador.com/busca.php?q=${nome}`).then( tod => {
-  const $ = cheerio.load(tod.data)  
-  var postagem = [];
-$("div.thought-card.mb-20").each((_, say) => {
-    var frase = $(say).find("p").text().trim(); 
-    var compartilhamentos = $(say).find("div.total-shares").text().trim(); 
-    var autor = $(say).find("a").text().split("\n")[0];
-    var imagem = $(say).find("div.sg-social-hidden.sg-social").attr('data-media');
-    var resultado = {
-      autor: autor,
-      compartilhamentos: compartilhamentos,      
-      imagem: imagem,
-      frase: frase
-    }
-    postagem.push(resultado)
-  })
-//  console.log(tod.data)
-  resolve(postagem)
-  }).catch(reject)
-  });
-}
-
-async function getMedia(url){
-    let img = await axios.get(url,{responseType:"arraybuffer"})
-    return img.data
-}
-
-const getBuffer = async (url, options) => {
-  try {
-    options ? options : {}
-    const res = await axios({
-      method: "get",
-      url,
-      headers: {
-        'DNT': 1,
-        'Upgrade-Insecure-Request': 1
-      },
-      ...options,
-      responseType: 'arraybuffer'
-    })
-    return res.data
-  } catch (e) {
-    console.log(`Error : ${e}`)
-  }
-}
-  
 (async () => {
+    console.log("login iniciado")
 
+    if (stateExists()) {
+        await ig.state.deserialize(loadState());
+    }
 
-if (stateExists()) {
-await ig.state.deserialize(loadState());
-}
-let loggedInUser
-try{
-loggedInUser = await ig.account.currentUser()
-console.log("conectado")
-}catch(err){
-console.log(err)
-loggedInUser = await ig.account.login(bot,senha)
-const serialized = await ig.state.serialize();
-delete serialized.constants; 
-saveState(serialized);
-}
+    let loggedInUser
+    try{
+        loggedInUser = await ig.account.currentUser()
+    }catch(err){
+        console.log(err)
+        loggedInUser = await ig.account.login("segui_eu_de_volta_pfv","34615194")            
+        const serialized = await ig.state.serialize();
+        delete serialized.constants; 
+        saveState(serialized);
+    }
+    
+    console.log("Logado com sucesso na conta: " + loggedInUser.username)
+    
+const threads = await ig.feed.directInbox().request()        
 let meID = loggedInUser.pk
-let threads = await ig.feed.directInbox().request()
-
-
-
-
-//postar
-//schedule.scheduleJob("0 0 */1 * * *", async () => { 
-/*const ArrayTema = ["motivação","rimas","mentalidade","reflexões","poesia","músicas","lírico","dinheiro","inspiração"]
-let tema = ArrayTema[Math.floor(Math.random() * ArrayTema.length)]
-//imagem
-pensador(tema).then(async resultado => { 
-let somenteum = resultado[Math.floor(Math.random() * resultado.length)]
-//publicando...
-postarfoto(somenteum.imagem, somenteum.frase + "\n\n#frases #motivação #diadia #fy #enriquece\n\nolhe a minha bio e te ensinarei a crescer 🤝")
-})
-console.log("imagem publicada")
-})*/
-
-
+    
     ig.realtime.on("receive",async(t,message) => {
         let msg = message[0]
-       // console.log(msg)
-           if(msg.topic.id == "135" && msg.topic.path == "/ig_sub_iris_response"){
+       if(msg.topic.id == "135" && msg.topic.path == "/ig_sub_iris_response"){
             let p_threads = (await ig.feed.directPending().request()).inbox.threads
             for(let i = 0;i<p_threads.length;i++){
                 let id = p_threads[i].thread_id
                 console.log(id)
                 try{
                     await ig.directThread.approve(id)
-                    await ig.entity.directThread(id).broadcastText("opa tudo bem?, mande qualquer pergunta em áudio e eu irei le responder 😁")
+                    await ig.entity.directThread(id).broadcastText("oii, blz ja segue eu aii, to te seguindo ja :), postarei memes, entre muitas outras coisas...")
                 }catch(err){
                     console.log(err)
                 }
             }
         }
-    })
+    })  
+    
+    
 
-ig.realtime.on("message",async (message)=> { 
-if(message?.message?.user_id == meID) return
-const budy = message.message.text
-const sender = message.message.user_id
-const rg = await verificaAll(sender)
-const rgcheck = await veriNumero(sender)
-const isOwner = rg.admin == "sim"
+  const seguidores_via_id = await ig.feed.accountFollowers("26669533");
+  const e = async () => {
+  let items = await seguidores_via_id.items();
+  let tema = items[Math.floor(Math.random() * items.length)]
+  ig.friendship.create(tema.pk_id);
+    console.log("seguindo: " + tema.username)
+    const temais = await seguidores_via_id.isMoreAvailable();
+      if (temais) {
+        await delay(20 * 1000)
+        e();        
+      } else {
+      return
+      }
+  };
+  
 
-const ia_onff = usuario.findOne({ ayu: "ayu" });
-console.log(ia_onff)
-   		//antispam
-if (budy && antiSpam.isFiltered(sender)) {
-return reply('「 SPAM 」Espere 5 segundos para usar a IA')}
-}        
+setInterval(() => e(), 20 * 1000)
 
-
-if (budy && !isOwner) antiSpam.addFilter(sender)
-
-console.log(message.message.item_type)
-const postarfoto = async (img, legenda) => {       
-const imageBuffer = await get({
-url: img,
-encoding: null, 
-})
-await ig.publish.photo({
-file: imageBuffer,
-caption: legenda,
-})
-};
-
-const reply = (teks) => {
-ig.realtime.direct.sendText({text:teks,threadId:message.message.thread_id,reply:{item_id:message.message.item_id,client_context:message.message.client_context}})                                        
-};
-
-const replyimg = async (link) => {
-ig.entity.directThread(message.message.thread_id).broadcastPhoto({file: await getBuffer(link)})
-};
-
-
-if (budy == "/ligar" && isOwner) {
-  bot_inf.updateOne({ ia: "desligada" }, { ia: "ligada" } })
-  reply("inteligência ligada!!")
-    }
-
-if (budy == "/desligar" && isOwner) {
-  bot_inf.updateOne({ ia: "ligada" }, { ia: "desligada" } })
-  reply("inteligência desligada!!")
-}
-
-if (budy && !budy.includes('rg') && !rgcheck){ 
-reply(`opa amigão parece que você não esta registrado, mande a segunite mensagem: rg, e efetue seu registro para continuar 😉`)
-return
-}
-
-if (budy == "rg") {
-if (rgcheck) return reply("você  ja esta registrado, mande algum audio para eu le responder...")
-let dt = await ig.user.info(sender)
-let cpfgerado = await randomNumber(10)
-gerarUsuario(dt.username, sender,cpfgerado)
-reply(`${dt.username} seu registro foi postado! 😇`)
-postarfoto(dt.hd_profile_pic_url_info.url, `@${dt.username} agora e um de nós, seja bem vindo!! 🥳`)
-}
-
-if (message.message.item_type == "voice_media" && ia_onff.ia == "ligada") {
-try {
-//console.log(message.message.voice_media.media.audio.audio_src)
-reply("sua mensagem foi recebida, aguarde a resposta...");
-const oggPath = await oggToMp3.createOgg(message.message.voice_media.media.audio.audio_src, message.message.item_id);
-const mp3Path = await oggToMp3.convertToMp3(oggPath, message.message.item_id);
-const openStrem = await openai.createTranscription(createReadStream(mp3Path), "whisper-1");
-const text = openStrem.data.text;
-await reply(`oque entendi no áudio que você mandou: ${text}`);
-/*if (text.includes("Arte" || "arte")) {
-await reply("criando sua arte 🥳...");
-const response = await openai.createImage({
-prompt: text,
-n: 1,
-size: "512x512",
-});
-replyimg(response.data.data[0].url)
-}else{*/
-await reply("buscando respostas 🥳...");
-const response = await openai.createCompletion({
-model: "text-davinci-003",
-prompt: text,
-temperature: 0.7,
-max_tokens: 500,
-stop: ["Ai:", "Human:"],
-top_p: 1,
-frequency_penalty: 0.2,
-presence_penalty: 0,
-})
-reply(response.data.choices[0].text.trim())
-removeStreams(mp3Path);
-//}
-}catch{
-reply("parece que o limit acabou, fale com o criador do bot, @breno.online")
-}
-} else if(message.message.item_type == "text" && ia_onff.ia == "ligada" && !budy.includes('/desligar')) {
-try {
-await reply("buscando respostas 🥳...");
-const response = await openai.createCompletion({
-model: "text-davinci-003",
-prompt: budy,
-temperature: 0.7,
-max_tokens: 500,
-stop: ["Ai:", "Human:"],
-top_p: 1,
-frequency_penalty: 0.2,
-presence_penalty: 0,
-})
-reply(response.data.choices[0].text.trim())
-}catch{
-reply("parece que o limit acabou, fale com o criador do bot, @breno.online")
-}
-}
-
-
-})
 ig.realtime.on('error', console.error);
 ig.realtime.on('close', () => console.error('RealtimeClient parou :('));
 await ig.realtime.connect({irisData: threads})
-})()
+})()  
 
 app.get('/', async (req, res) => {
-  res.sendFile('index.html', { root: __dirname })
+  res.send('oiiii')
 });
 
 const porta = process.env.PORT || 5000;
 //iniciando...
-
-app.listen(porta, () => {
-  console.log(`Aplicativo radando em: http://localhost:${porta}`);
-  schedule.scheduleJob('* * * * *', () => { 
-    bot_inf.findOne({ayu: 'ayu'}).then(async (botInfo) => {
-    if (!util) {
-    addBotinfo()
-    console.log(botInfo)
-   }
-   })
-  });
-});
+app.listen(porta, () => console.log("site Online na porta:", porta));
